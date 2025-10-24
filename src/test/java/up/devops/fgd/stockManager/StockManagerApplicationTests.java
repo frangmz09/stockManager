143c281f77c81c8ad8938bf91e787f031013dbb3
fran <frangomez0409@gmail.com>
2025-10-24 14:31:58 -0300

    config: Configuración para rama develop de Tests

--- src/test/java/up/devops/fgd/stockManager/StockManagerApplicationTests.java
+++ src/test/java/up/devops/fgd/stockManager/StockManagerApplicationTests.java
@@ -2,8 +2,10 @@ package up.devops.fgd.stockManager;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StockManagerApplicationTests {
 
 	@Test
--- src/test/java/up/devops/fgd/stockManager/controller/ProductControllerIntegrationTest.java
+++ src/test/java/up/devops/fgd/stockManager/controller/ProductControllerIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import up.devops.fgd.stockManager.model.Product;
 import up.devops.fgd.stockManager.repository.ProductRepository;
@@ -15,7 +16,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class ProductControllerIntegrationTest {
+@ActiveProfiles("test")
+public class ProductControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
